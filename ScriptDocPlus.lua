323ab81748c86915febb78bf4bb82113ee1b1a00
MightyPart <cameron.campbell325@gmail.com>
2023-08-01 10:22:22 +0100

    Update ScriptDocPlus.lua

--- ScriptDocPlus.lua
+++ ScriptDocPlus.lua
@@ -1,6 +1,6 @@
 local ScriptDocPlus = {}; ScriptDocPlus.__index = ScriptDocPlus
 
-function ScriptDocPlus:ClampLineNumber(lineNum:int)
+function ScriptDocPlus:ClampLineNumber(lineNum:number)
 	return math.clamp(lineNum, 1, self:GetLineCount())
 end
 
@@ -10,7 +10,7 @@ function ScriptDocPlus:Paste(text, lineNum, fromCurrent)
 	self:EditTextAsync(text, lineNum, 1, lineNum, string.len(line)+1)
 end
 
-function ScriptDocPlus:GetLine(lineNum:int, fromCurrent:boolean)
+function ScriptDocPlus:GetLine(lineNum:number, fromCurrent:boolean)
 	if fromCurrent == true then lineNum = self:GetSelection()+(lineNum or 0) end
 	if lineNum ~= nil then lineNum = self:ClampLineNumber(lineNum) end
 	return self.document:GetLine(lineNum), lineNum
@@ -50,7 +50,7 @@ end
 function ScriptDocPlus:GetSelectionStart()
 	return self.document:GetSelectionStart()
 end
-function ScriptDocPlus:GetText(startLine:int, startCharacter:int, endLine:int, endCharacter:int)
+function ScriptDocPlus:GetText(startLine:number, startCharacter:number, endLine:number, endCharacter:number)
 	return self.document:GetText(startLine, startCharacter, endLine, endCharacter)
 end
 function ScriptDocPlus:GetViewport()
@@ -68,10 +68,10 @@ end
 function ScriptDocPlus:EditTextAsync(newText:string, startLine:number, startCharacter:number, endLine:number, endCharacter:number)
 	return self.document:EditTextAsync(newText, startLine, startCharacter, endLine, endCharacter)
 end
-function ScriptDocPlus:ForceSetSelectionAsync(cursorLine:number, cursorCharacter:number, anchorLine:int, anchorCharacter:int)
+function ScriptDocPlus:ForceSetSelectionAsync(cursorLine:number, cursorCharacter:number, anchorLine:number, anchorCharacter:number)
 	return self.document:ForceSetSelectionAsync(cursorLine, cursorCharacter, anchorLine, anchorCharacter)
 end
-function ScriptDocPlus:RequestSetSelectionAsync(cursorLine:number, cursorCharacter:number, anchorLine:int, anchorCharacter:int)
+function ScriptDocPlus:RequestSetSelectionAsync(cursorLine:number, cursorCharacter:number, anchorLine:number, anchorCharacter:number)
 	return self.document:RequestSetSelectionAsync(cursorLine, cursorCharacter, anchorLine, anchorCharacter)
 end
 -- ==========================================================================================================
